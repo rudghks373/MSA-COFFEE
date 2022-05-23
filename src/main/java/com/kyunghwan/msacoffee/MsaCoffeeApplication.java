49512f4fd7f6deb3c9225dec214ec372c9f8f6ca
rudghks373 <rudghks373@naver.com>
2022-05-24 00:45:53 +0900

    init project

---
+++ src/main/java/com/kyunghwan/msacoffee/MsaCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.kyunghwan.msacoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaCoffeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaCoffeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kyunghwan/msacoffee/MsaCoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyunghwan.msacoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaCoffeeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
